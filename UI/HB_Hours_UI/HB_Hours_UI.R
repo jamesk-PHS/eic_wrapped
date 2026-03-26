684177f0c500fb6ba79b15ac9c870b2e593f4ba1
James <james.kilgour2@phs.scot>
2026-03-26 16:12:26 +0000

    Made this page dynamic

--- UI/HB_Hours_UI/HB_Hours_UI.R
+++ UI/HB_Hours_UI/HB_Hours_UI.R
@@ -11,24 +11,9 @@ HB_Hours <- page_sidebar(
     full_screen = TRUE,
     ## HEADLINE FIGURES
     layout_columns(max_height = "250px", 
-                   value_box(
-                     title = "Total WTE",
-                     value = textOutput("total_WTE"),
-                     theme = phsstyles::phs_colors()[1],
-                     p("The total baseline hours your team worked")
-                   ),
-                   value_box(
-                     title = "Total B&A",
-                     value = textOutput("total_SSUBA"),
-                     theme = phsstyles::phs_colors()[2],
-                     p("The total bank and agency hours your team had")
-                   ),
-                   value_box(
-                     title = "Total E&O",
-                     value = textOutput("total_SSUEO"),
-                     theme = phsstyles::phs_colors()[4],
-                     p("The total excess and overtime hours your team worked")
-                   )
+                   uiOutput("Total_WTE"),
+                   uiOutput("Total_BA"),
+                   uiOutput("Total_EO")
     ),
     ## MAIN PLOTS
     uiOutput("WTE_hours")
